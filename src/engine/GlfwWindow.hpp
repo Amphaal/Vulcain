9b994e08d4fc3694cb53c2006e4c894e492902e3
Amphaal <guillaume.vara@gmail.com>
2021-03-06 21:04:44 +0100

    update deps + move defines in cmake script

--- src/engine/GlfwWindow.hpp
+++ src/engine/GlfwWindow.hpp
@@ -22,10 +22,7 @@
 #include <atomic>
 #include <iostream>
 
-#define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
-
-#define GLFW_EXPOSE_NATIVE_WIN32
 #include <GLFW/glfw3native.h>
 
 #include "IDrawer.h"
